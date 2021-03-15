25833cf3f8b079f0e9969c5bff7aa7370563e8d9
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-15 11:38:44 -0500

    did the install, looking through files, took out the footer

--- database.sql
+++ database.sql
@@ -6,5 +6,6 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+     "authority" VARCHAR (80) NOT NULL DEFAULT 'USER'
 );
