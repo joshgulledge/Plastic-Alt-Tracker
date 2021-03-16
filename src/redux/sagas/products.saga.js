b6585d43ab0f887643ecd5a4702097eb3ed7875d
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-16 14:51:58 -0500

    the add product pinfo gets sent to the db

--- src/redux/sagas/products.saga.js
+++ src/redux/sagas/products.saga.js
@@ -3,7 +3,26 @@ import axios from 'axios';
 
 function* productSaga () {
   yield takeEvery('GET_PRODUCT', getProduct)
+  yield takeEvery('ADD_PRODUCT', addProduct);
 }; // end productSaga
+
+const addProduct = function* (action) {
+  try {
+    console.log('in add product saga....');
+    // send to the server
+    yield axios.post('/api/products',{newProduct: action.payload});
+
+    // update all product list
+    yield put({
+      type: 'GET_PRODUCT'
+    });
+    
+  }
+  catch (err) {
+    console.log('uh oh, we got a problem 💥 :', err);
+  }
+}; // end addProduct
+
 
 const getProduct = function* () {
   try {
