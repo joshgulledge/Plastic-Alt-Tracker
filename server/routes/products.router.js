647b47bea77d6ebf92f7fb79a92354df882d0cfc
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-15 20:40:59 -0500

    the server get route is functional, tested with postman

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -2,12 +2,19 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
 router.get('/', (req, res) => {
-  // GET route code here
-});
+  // GET the products from the Database
+  const SQLtext = `SELECT * FROM "products";`
+
+  pool.query(SQLtext).then(dbRes => {
+    // send the results back
+    res.send(dbRes.rows);
+  }).catch(err => {
+    // show the error
+    console.log('💥 Error! ', err);
+    res.sendStatus(500);
+  });
+}); // end the get products route
 
 /**
  * POST route template
