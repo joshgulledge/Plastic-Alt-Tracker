0d1b45c6f0d791df589d4fa9bd134416c3793eef
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-18 18:53:53 -0500

    user liked products display on like page

--- src/components/UserLikes/UserLikes.jsx
+++ src/components/UserLikes/UserLikes.jsx
@@ -1,31 +1,48 @@
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const UserLikes = function () {
   // get the likes from redux
   const likedList = useSelector(store => store.products.userPreference); // contains id of likes
   const allProducts = useSelector(store => store.products.productList); // contains all products
-
   // this will be the list of liked products
-  const LikedProductList = [];
+  const [likedProductList, setLikedProductList] = useState([]);
+
+  console.log('liked list is ', likedList);
+  console.log('all product is ', allProducts);
 
   const makeProductList = function () {
-    likedList.map()
-  }
+    // loop through the lists and if the product is in the liked list, add to liked product array
+    const results = [];
+    likedList.map(like => {
+      // check if its a like
+     if (like.user_preferences === 2) return;
+
+     const [result] = allProducts.filter(product => product.id === like.product_id);
+     results.push(result);
+    }); // end like forEach
+
+    setLikedProductList(results);
+  }; // end makeProductList
 
   useEffect(() => {
     // when the page loads, compare the two list and make an array with only the liked products
     makeProductList();
   }, []);
 
-  
-  console.log(LikedProducts);
+  console.log(likedProductList);  
 
   return (
     <div>
       <h3>
         This page will display the liked products 😎
       </h3>
+
+      {likedProductList.map(product => {
+        return (
+          <img src={product.image_url} width='20%' />
+        )
+      })}
 
     </div>
   )
