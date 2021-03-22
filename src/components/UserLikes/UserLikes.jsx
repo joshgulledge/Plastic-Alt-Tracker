903a84c4478f70aa346777b9cd6a0a9d6c825bd7
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-22 17:12:33 -0500

    now i have the user preference reason on the like page

--- src/components/UserLikes/UserLikes.jsx
+++ src/components/UserLikes/UserLikes.jsx
@@ -14,18 +14,47 @@ const UserLikes = function () {
   // this will be the list of liked products
   const [likedProductList, setLikedProductList] = useState([]);
 
+  // const makeProductList = function () {
+  //   // loop through the lists and if the product is in the liked list, add to liked product array
+  //   const results = [];
+  //   likedList.map(like => {
+  //     // check if its a like, 2 represents hated products
+  //    if (like.user_preferences === 2) return;
+  //     // the filter returns the object that matches into an array,
+  //    const [result] = allProducts.filter(product => product.id === like.product_id);
+  //     // i got the object out of the array and pushed it into another one
+  //    results.push(result);
+  //   }); // end like forEach
+  //   setLikedProductList(results);
+  // }; // end makeProductList
+
   const makeProductList = function () {
-    // loop through the lists and if the product is in the liked list, add to liked product array
     const results = [];
-    likedList.map(like => {
-      // check if its a like, 2 represents hated products
-     if (like.user_preferences === 2) return;
-      // the filter returns the object that matches into an array,
-     const [result] = allProducts.filter(product => product.id === like.product_id);
-      // i got the object out of the array and pushed it into another one
-     results.push(result);
-    }); // end like forEach
-    setLikedProductList(results);
+    // loop through preference list
+    likedList.map( preference => {
+      // if its not a like return immediatly
+      if (preference.user_preferences === 2) return;
+      console.log('the liked list loop');
+
+      // loop through the product list
+      allProducts.forEach( product => {
+        console.log('the all products loop');
+        // where the ids match add that product info to a new obj
+        if ( product.id === preference.product_id) {
+          console.log('the ids match');
+          const newObj = {
+            name: product.brand,
+            image: product.image_url,
+            id: product.id,
+            reason: preference.reason
+          };
+          console.log('new obj created', newObj);
+          // add that new obj to a new array of obj
+          results.push(newObj)
+        }; // end if match
+      }); // end all products loop
+    }); // end the liked list loop
+    setLikedProductList(results)
   }; // end makeProductList
 
   useEffect(() => {
@@ -50,7 +79,11 @@ const UserLikes = function () {
       {likedProductList.map(product => {
         return (
           <div key={product.id}>
-            <img src={product.image_url} width='20%' onClick={() => imageClick(product)} />
+            <img src={product.image} width='20%' onClick={() => imageClick(product)} />
+            <span>
+              {product.reason}
+            </span>
+
           </div>
         )
       })}
